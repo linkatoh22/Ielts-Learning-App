553ee16ad4a4c8a62515e668f4865abc5f70ac14
linkatoh22 <lintoh111@gmail.com>
2025-08-28 17:32:10 +0700

    Add Reading test Listening test

--- backend/models/listeningExamModel.js
+++ backend/models/listeningExamModel.js
@@ -2,34 +2,29 @@ const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
 
-const TFNQuestionSchema = new mongoose.Schema({
+const CompletePassage= new mongoose.Schema({ //Complete Passage 
     qid: { type: String, default: uuidv4 }, 
-    questionText:{ type: String, required: true },
-    answer: { type: String, enum: ["TRUE", "FALSE", "NOT GIVEN"] }
-})
-
-const YNNQuestionSchema = new mongoose.Schema({
-    qid: { type: String, default: uuidv4 }, 
-    questionText:{ type: String, required: true },
-    answer: { type: String, enum: ["YES", "NO", "NOT GIVEN"] }
-}) 
-
-const MatchingQuestionMatching= new mongoose.Schema({ //MATCHING 
-    qid: { type: String, default: uuidv4 }, 
-    questionText:{ type: String, required: true },
     answer: { type: String }
 }) 
 
-const MCQuestionMatching= new mongoose.Schema({ //MULTIPLE CHOICE
+
+const CompleteSentences= new mongoose.Schema({ //CHOOSE ANSWER
+    qid: { type: String, default: uuidv4 }, 
+    CompleteSentences:{ type: String, required: true },
+    answer: { type: String }
+}) 
+
+
+const ChooseAnswer= new mongoose.Schema({ //MULTIPLE CHOICE
     qid: { type: String, default: uuidv4 }, 
     questionText:{ type: String, required: true },
     optionText:[{ type: String, required: true }],
     answer: { type: String, enum: ["A", "B", "C","D"] }
 }) 
 
-const MatchingCompletePassage= new mongoose.Schema({ //Complete Passage 
+const MarkAnswer= new mongoose.Schema({ //CHOOSE ANSWER
     qid: { type: String, default: uuidv4 }, 
-    answer: { type: String }
+    answer:[ { type: String }]
 }) 
 
 const questionSchema = new mongoose.Schema({
@@ -37,38 +32,35 @@ const questionSchema = new mongoose.Schema({
     type: String, 
     enum: [
       
-      "True / False/ Not Given",
-      "Yes /No / Not Given",
-      "Matching",
-      "Multiple Choice",
-      "Complete Passage"
+      "Complete Passage", //Hinh anh optional
+      "Choose Answer",
+      "Complete Sentences", //Hinh anh optional
+      "Complete Sentences Table", //Hinh anh optional
+      "Mark Answer"
     ],
     required: true
   },
     questionGuideline : {type:String},
     questionDescription : {type:String},
 
-    questionPassage : {type:String},
+    questionPassage : {type:String}, //Complete Passage
 
     questionMatchOption:[{ type: String }], //Matching
 
-    questionTFN : [TFNQuestionSchema],
-    questionYNN : [YNNQuestionSchema],
-    questionMatching : [MatchingQuestionMatching],
-    questionMultipleChoice : [MCQuestionMatching],
+    questionTextMA :[{ type: String, required: true }], ///Matching Answer
 
-    questionCompletePassage : [MatchingCompletePassage],
+    questionCP: [CompletePassage],
+    questionCA : [ChooseAnswer],
+    questionCS : [CompleteSentences],
 
+    questionMA : [MarkAnswer],
 
-    // detail: { type: mongoose.Schema.Types.Mixed, required: true } // chứa dữ liệu theo type
 });
 
 const audioSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  questionsPartOne: questionSchema,
-  questionsPartTwo: questionSchema,
-  questionsPartThree: questionSchema
+  title: { type: String},
+  audioSrc: { type: String, required: true },
+  questionsPart: [questionSchema]
 });
 
 const listeningTestSchema = new mongoose.Schema({

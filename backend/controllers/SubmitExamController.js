e12af3d9210732aeedc657c3914535d4dd16e19b
linkatoh22 <lintoh111@gmail.com>
2025-08-28 18:24:26 +0700

    Add: Listening Test Submit

--- backend/controllers/SubmitExamController.js
+++ backend/controllers/SubmitExamController.js
@@ -1,5 +1,8 @@
 const { ReadingTest, PassageReading } = require("../models/readingExamModel.js");
 const ReadingSubmission = require("../models/submissionReadingModel.js");
+const ListeningSubmission  = require("../models/submissionListeningModel.js")
+const {ListeningTest, audioListening } = require("../models/listeningExamModel.js")
+
 
 const submitReadingTest = async (req, res) => {
   try {
@@ -80,4 +83,81 @@ const submitReadingTest = async (req, res) => {
   }
 };
 
-module.exports = { submitReadingTest };
+
+const submitListeningTest = async (req, res) => {
+  try {
+    
+    const { id } = req.params; // readingTestId
+    const { userId, answers } = req.body; 
+    const listeningTest = await ListeningTest.findById(id).populate("passages");
+
+
+    if (!listeningTest) {
+      return res.status(404).json({ message: "Listening test not found" });
+    }
+
+    const audio = await audioListening.findById(listeningTest.audio);
+    
+    if (!audio) {
+      return res.status(404).json({ message: "Listening Audio not found" });
+    }
+
+    
+    // Gom hết question có đáp án
+
+    // const allQuestions = [
+    //   ...audio.questionsPart?.questionCP || [],
+    //   ...audio.questionsPart?.questionCA || [],
+    //   ...audio.questionsPart?.questionCS || [],
+    // ];
+    
+    const allQuestions = () => {
+      return audio.questionsPart
+        .map((item) => [
+          ...(item.questionCP || []),
+          ...(item.questionCA || []),
+          ...(item.questionCS || []),
+        ])
+        .flat();
+    };
+    // Tạo map questionId -> answer đúng
+    const correctAnswers = {};
+    allQuestions.forEach(q => {
+      correctAnswers[q._id.toString()] = q.answer;
+    });
+
+    
+    // Chấm điểm
+    let score = 0;
+    answers.forEach(ans => {
+      const correct = correctAnswers[ans.questionId];
+      
+      if (correct && correct.toLowerCase().trim() === ans.answer.toLowerCase().trim()) {
+        score++;
+      }
+    });
+
+    // Lưu kết quả
+    const submission = new ListeningSubmission({
+      userId,
+      listeningTestId: id,
+      answers,
+      score
+    });
+
+    await submission.save();
+
+    res.status(200).json({
+      message: "Submitted successfully",
+      score,
+      total: allQuestions.length,
+      submission
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { submitReadingTest,submitListeningTest };

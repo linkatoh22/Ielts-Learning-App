7b3fd205c2af84b539413b16a702670286ec6c04
linkatoh22 <lintoh111@gmail.com>
2025-08-28 23:40:20 +0700

    Fix: Reading,Listening API

--- backend/models/submissionReadingModel.js
+++ backend/models/submissionReadingModel.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const answerSchema = new mongoose.Schema({
   questionId: { type: mongoose.Schema.Types.ObjectId, required: true }, // tham chiếu tới câu hỏi trong passage
-  answer: { type: String, required: true } // đáp án mà user chọn/nhập
+  answer: { type: String}, // đáp án mà user chọn/nhập
+  correctAnswer:{ type: String },
+  isCorrect:{type:Boolean}
 });
 
 const readingAttemptSchema = new mongoose.Schema({
